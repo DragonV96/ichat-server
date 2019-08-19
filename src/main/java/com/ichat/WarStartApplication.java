75d6936db83e0e13f1502d1381dae1e9e38fb090
glw <guolongwei@comtom.cn>
2019-08-19 16:55:44 +0800

    【修改】修改为war包打包启动

---
+++ src/main/java/com/ichat/WarStartApplication.java
@@ -0,0 +1,17 @@
+package com.ichat;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Create by glw
+ * 2018/12/16 18:01
+ * 使用外置tomcat启动application
+ */
+public class WarStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+}
