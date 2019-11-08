7d6b6eea1085a180995d2500c934cba76385d572
glw <guolongwei@comtom.cn>
2019-11-08 16:25:57 +0800

    【修改】修改后台服务重启脚本

--- script/ichat/re_install_ichat.sh
+++ script/ichat/re_install_ichat.sh
@@ -3,7 +3,7 @@ docker rm -f ichat
 
 docker rmi -f ichat:1.0.0
 
-docker load -i ichat.tar
+docker build -t ichat:1.0.0 /usr/local/apps/ichat
 
 docker run --name ichat --net=host --restart=always -m 1024m -e JAVA_OPTIONS="-Xmx768m" -e appName="ichat.jar" --privileged=true -e serverPort=8080 -v /usr/local/apps/ichat/conf:/data/apps/config -v /usr/local/apps/ichat/log:/data/log -d ichat:1.0.0
 
