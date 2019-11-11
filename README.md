38afbe0a1e410d45e4dd803338a0c071e895a226
glw <guolongwei@comtom.cn>
2019-11-11 16:54:24 +0800

    【修改】更新README windows搭建和linux部署流程

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 <h1><center>跨平台仿微信即时通讯聊天app</center></h1>
 
-如果本项目对你有用，还请在项目右上角点个star~，谢谢大佬支持。
+如果本项目对你有用，还请在项目右上角点个star，谢谢大佬支持。
 
 # 作品简介
 
-​		本项目采用前后端分离开发，面向restful风格的接口编程。
+本项目采用前后端分离开发，面向restful风格的接口编程。
 
-​		前端可跨平台在web，android，ios等设备上运行，图片服务器用的fastdfs统一管理app内图片，登录以及消息转发服务器采用netty+springboot，快速高效开发的同时，服务能承受大量在线同时进行聊天的后台，采用nginx进行反向转发代理，提高服务器稳定性。
+前端可跨平台在web，android，ios等设备上运行，图片服务器用的fastdfs统一管理app内图片，登录以及消息转发服务器采用netty+springboot，快速高效开发的同时，服务能承受大量在线同时进行聊天的后台，采用nginx进行反向转发代理，提高服务器稳定性。
 
-​		采用docker容器方式部署，隔离开发与测试环境不一致问题。
+采用docker容器方式部署，隔离开发与测试环境不一致问题。
 
 # 涉及技术框架
 
@@ -49,10 +49,95 @@
 - 数据库 MariaDB/mysql HikariCP 2.7.9
 - 文件服务器 nginx 1.12.0 fastdfs 5.05
 - 前端框架 MUI H5plus
+- 简化对象封装工具Lombok
 
-# 搭建流程
+# Windows搭建流程
 
-​		稍后有空再更，script文件夹下有部署脚本。
+1）可先执行下方一遍linux部署流程（可跳过）
+
+2）项目导入idea（此操作若不会请百度）
+
+3）将 `application.yml` 中的  private 值改为自己mysql服务器ip，mysql的password改为自己的密码。
+
+![1573461402678](assets/1573461402678.png)
+
+4）搭建一个fastdfs（推荐使用docker搭建，搭建在centos7上），依次执行下述步骤
+
+- 将项目下的 `script/deploy/fastdfs` 文件夹（fastdfs）通过Xftp工具（其他类似工具也可以）拷入到服务器任意位置
+- 依次执行下述5条命令（下方第四条命令的 `你的服务器IP` 替换为你的服务器本地IP，**如何查看本地IP请看linux部署流程 第七步**）
+
+````
+cp -r fastdfs /usr/local/
+
+docker pull season/fastdfs
+
+docker tag season/fastdfs:5.11 fastdfs-nginx:v5.11
+
+sed -i -e "s|tracker_server=.*:22122|tracker_server=你的服务器IP:22122|g" /usr/local/fastdfs/conf/storage.conf
+
+docker run --restart=always -m 512m -v /usr/local/fastdfs/storage:/data/fastdfs/storage -v /usr/local/fastdfs/tracker:/data/fastdfs/tracker -v /usr/local/fastdfs/conf/storage.conf:/etc/fdfs/storage.conf -v /usr/local/fastdfs/conf/tracker.conf:/etc/fdfs/tracker.conf -v /usr/local/fastdfs/conf/client.conf:/etc/fdfs/client.conf -v /usr/local/fastdfs/conf/mod_fastdfs.conf:/etc/fdfs/mod_fastdfs.conf --name fastdfs -d --net=host --privileged=true fastdfs-nginx:5.11
+````
+
+5）运行项目的 `Application.java` 文件启动。
+
+# linux部署流程
+
+**搭建前请确保自己有一个centos 7以上的服务器，且服务器已经提前装好docker，否则一键部署脚本会出错**
+
+**配置文件无需自行修改ip，一键部署脚本中会涉及此操作**
+
+1）修改项目中 `application.yml` 的数据库配置，如图
+
+- `${host.private}` 改为 `${host.local}`
+- password改为自己的密码
+
+![1573461647085](assets/1573461647085.png)
+
+2）用maven执行install操作，打包项目。
+
+![1573460283114](assets/1573460283114.png)
+
+3）将项目里的 `application.yml` `Dockerfile` 与打包好的 `jar包` 一起放入项目的 `script/deploy` 文件夹下。
+
+4）将项目下的 `script/deploy` 文件夹通过Xftp工具（其他类似工具也可以）拷入到服务器任意位置
+
+5）使用XShell（其他类似工具也可以）进入你拷进去的 `deploy` 文件夹下，执行
+
+````
+sudo chmod +x install_online.sh
+sudo chmod +x delete.sh
+sudo chmod +x restart.sh
+````
+
+6）然后执行安装命令
+
+````
+./install_online.sh
+````
+
+7）执行过程中会出现暂停，并且提示输入IP，此时输入服务器的本地IP即可（另起一个Xshell查看本机ip，命令如下，第二条显示的对应ip即为当前服务器ip）输入完成等待执行完毕即可。
+
+````
+ip addr
+````
+
+![1573461124719](assets/1573461124719.png)
+
+## 安装失败
+
+执行删除所有容器及镜像脚本**（此操作会删除所有docker容器及镜像，若服务器存在其他容器及镜像请勿进行此操作）**
+
+````
+./delete.sh
+````
+
+## 重启容器
+
+执行重启所有容器脚本
+
+````
+./restart.sh
+````
 
 # 界面展示
 
---
+++ script/deploy/install_online.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+# 部署安装脚本
+
+echo "*************************部署开始*************************"
+# 安装前环境配置
+echo "=============正在关闭selinux...========================="
+setenforce 0
+sed -i -e "s|^[^#]SELINUX=.*|SELINUX=disabled|" /etc/selinux/config
+
+echo "=============防火墙开机自启...========================="
+systemctl enable firewalld
+echo "=============开放ssh的防火墙端口22...========================="
+firewall-cmd --zone=public --add-port=22/tcp --permanent
+echo "=============开放mysql的防火墙端口3306...========================="
+firewall-cmd --zone=public --add-port=3306/tcp --permanent
+echo "=============开放ichat的防火墙端口8080...========================="
+firewall-cmd --zone=public --add-port=8080/tcp --permanent
+echo "=============开放fastdfs的防火墙端口80...========================="
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+echo "=============开放fastdfs的防火墙端口22122...========================="
+firewall-cmd --zone=public --add-port=22122/tcp --permanent
+echo "=============重启防火墙...========================="
+service firewalld restart
+
+#启动docker服务
+echo "=============启动docker服务...========================="
+systemctl start docker
+sudo systemctl enable docker
+
+#移动文件到指定位置
+echo "=============将fastdfs文件移动到指定位置...========================="
+cp -r fastdfs /usr/local/
+echo "=============将数据库sql文件移动到指定位置...========================="
+mkdir /usr/local/mysql/
+cp chat.sql /usr/local/mysql/
+echo "=============将yml文件移动到指定位置...========================="
+mkdir /usr/local/apps/ichat/conf/
+cp *.yml /usr/local/apps/ichat/conf/
+
+#拉取镜像文件
+echo "===============拉取mysql镜像文件...==============="
+docker pull mysql:5.7.26
+echo "===============拉取fastdfs镜像文件...==============="
+docker pull season/fastdfs
+docker tag season/fastdfs:5.11 fastdfs-nginx:v5.11
+echo "===============加载ichat镜像文件...==============="
+docker build -t ichat:1.0.0 .
+
+#检测ip
+function check_ip() {
+  local IP=$IP
+  VALID_CHECK=$(echo $IP|awk -F. '$IP<=255&&$2<=255&&$3<=255&&$4<=255{print "yes"}')
+  if echo $IP|grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"; then
+    if [ $VALID_CHECK == "yes" ]; then
+      echo "IP: $IP  为合法IP"
+      return 0
+    else
+      echo "IP: $IP  为不合法IP，请重新输入"
+      return 1
+    fi
+  else
+    echo "IP: $IP  格式错误，请重新输入"
+    return 1
+  fi
+}
+#直到输入合法的ip才继续
+while true; do
+  read -p "请输入服务器IP地址(input current linux IP address here,please)：" IP
+  check_ip $IP
+  [ $? -eq 0 ] && break
+done
+
+#修改fastdfs配置文件
+echo "===============修改fastdfs配置文件...==============="
+echo "输入的服务器IP地址为：" $IP
+sed -i -e "s|tracker_server=.*:22122|tracker_server=$IP:22122|g" /usr/local/fastdfs/conf/storage.conf
+
+#创建运行环境容器并启动
+echo "===============创建mysql容器并启动...==============="
+docker run --name mysql --net=host --restart=always -v /usr/local/mysql/conf:/etc/mysql/conf.d -v /usr/local/mysql/data:/var/lib/mysql -v /usr/local/mysql/log:/log -e MYSQL_ROOT_PASSWORD=root --privileged=true -d mysql:5.7.26 --lower_case_table_names=1
+
+echo "===============创建fastdfs容器并启动...==============="
+docker run --restart=always -m 512m -v /usr/local/fastdfs/storage:/data/fastdfs/storage -v /usr/local/fastdfs/tracker:/data/fastdfs/tracker -v /usr/local/fastdfs/conf/storage.conf:/etc/fdfs/storage.conf -v /usr/local/fastdfs/conf/tracker.conf:/etc/fdfs/tracker.conf -v /usr/local/fastdfs/conf/client.conf:/etc/fdfs/client.conf -v /usr/local/fastdfs/conf/mod_fastdfs.conf:/etc/fdfs/mod_fastdfs.conf --name fastdfs -d --net=host --privileged=true fastdfs-nginx:5.11
+
+sleep 40s
+#初始化mysql数据库（sql路径为宿主机）
+echo "===============初始化ichat数据库脚本ichat.sql...==============="
+docker exec -i mysql sh -c "exec mysql -uroot -proot" < /usr/local/mysql/chat.sql
+#创建ichat容器并启动
+echo "===============创建ichat容器并启动...==============="
+docker run --name ichat --net=host --restart=always -m 1024m -e JAVA_OPTIONS="-Xmx768m" -e appName="ichat.jar" --privileged=true -e serverPort=8080 -v /usr/local/apps/ichat/conf:/data/apps/config -v /usr/local/apps/ichat/log:/data/log -d ichat:1.0.0
+echo "===============修改ichat服务yml配置文件...==============="
+echo "输入的服务器IP地址为：" $IP
+sed -i -e "s|private: .*|private: $IP|g" /usr/local/apps/ichat/conf/application.yml
+echo "===============重启ichat容器使配置生效...==============="
+docker restart ichat
+echo "*************************部署完成*************************"
+
